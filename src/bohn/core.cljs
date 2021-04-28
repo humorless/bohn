24ecb76dc0952e417fed10d14744858410a19de8
Laurence Chen <humorless@gmail.com>
2021-04-29 04:41:41 +0800

    input data -> reagent atom

--- src/bohn/core.cljs
+++ src/bohn/core.cljs
@@ -3,12 +3,24 @@
    [reagent.core :as r]
    [reagent.dom :as d]))
 
+(def debug true)
+(defonce state (r/atom {:date "2021-04-25"}))
 ;; -------------------------
 ;; Views
 
+(defn handle-input-change
+  [entity-key e]
+  (swap! state
+         assoc entity-key (-> e .-target .-value))
+  (when debug
+    (prn @state)))
+
 (defn home-page []
   [:div [:h2 "Select a date"]
-   [:input {:type "date"}]])
+   [:input {:type "date"
+            :placeholder "2014-03-18"
+            :value (:date @state)
+            :on-change (partial handle-input-change :date)}]])
 
 ;; -------------------------
 ;; Initialize app
